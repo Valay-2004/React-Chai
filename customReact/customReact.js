3630e087105d3f64cdae5600fe26b3ea255c69e5
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-13 17:06:05 +0000

    OK for today

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -2,10 +2,22 @@
 const mainContainer = document.querySelector('#root');
 
 function customRender(reactElement, container){
+    /* // This may be not the correct way to create customRender
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
     domElement.setAttribute('href', reactElement.props.href);
     domElement.setAttribute('target', reactElement.props.target);
+    container.appendChild(domElement);
+    */
+
+    // this below is
+    // continuosly creates element
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    for(const prop in reactElement.props){
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     container.appendChild(domElement);
 }
 
