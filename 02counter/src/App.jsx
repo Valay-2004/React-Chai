5e64394513ea240ad3d2e28644baaf24b76ad057
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-21 07:33:38 +0000

    Trying to ad two buttons and change the UI

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let counter = 15;
+
+  const addValue = () => {
+    // console.log(`Value Added ${Math.floor(Math.random()*3)}`);
+    console.log("clicked", counter);
+    counter += 1;
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        Valay <sub>do</sub> react
+      </h1>
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button>Remove Value {counter}</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
