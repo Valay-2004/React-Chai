e9597d8a9384b5bd6061fada4d8487da5ab5f291
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-28 16:13:58 +0000

    Creating passwordGenerator using useCallback()

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
 
   const [length, setLength] = useState(0);
   const [numberAllowed, setNumber] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberAllowed) str += "0123456789";
+    if(charAllowed) str += "~!@#$%^&*()_+{[]}|<>/`";
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   return (
     <>
