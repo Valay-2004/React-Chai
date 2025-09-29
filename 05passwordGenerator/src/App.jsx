5ddf6f19ec49d02258abb274fe3a4543d46f5c8f
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-29 15:22:57 +0000

    Added Numbers and characters checkbox

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,9 +1,9 @@
 import { useCallback, useState } from "react";
-// import "./App.css";
+import "./App.css";
 
 function App() {
   const [length, setLength] = useState(0);
-  const [numberAllowed, setNumber] = useState(false);
+  const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
@@ -40,10 +40,46 @@ function App() {
             readOnly
           />
           <button
-          className="outline-none bg-blue-700 text-white px-3py0.5
-          shrink-0">
+            className="outline-none bg-blue-700 text-white px-3py0.5
+          shrink-0"
+          >
             copy
           </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }} />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
         </div>
       </div>
     </>
