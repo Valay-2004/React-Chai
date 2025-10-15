eda55e07818de7f7272d14fb4007d3d09b04cb1e
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-15 06:00:44 +0000

    Imported components and hooks

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
   return (
     <>
       <h1 className="text-3xl bg-orange-500 p-4">Currency Converter</h1>
