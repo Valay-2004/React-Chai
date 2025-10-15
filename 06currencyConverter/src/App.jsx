131a1a9adec16d14f65691cfb1e5c6d86f0310df
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-10-15 05:43:51 +0000

    Removed unused import

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1 className='text-3xl bg-orange-500 p-4'>Currency Converter</h1>
+      <h1 className="text-3xl bg-orange-500 p-4">Currency Converter</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
