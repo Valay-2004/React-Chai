feb7ef53ca644c6d95eed4046ff023b6586596ac
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-28 06:27:53 +0000

    Updated App with Home jsx

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 function App() {
   return (
     <>
       <Header />
       {/* <h1 className="bg-green-600 p-4 text-center">React Router</h1> */}
+      <Home />
       <Footer />
     </>
   );
