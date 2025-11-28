0eb918b045bbb923798f5432c9856b32907b8053
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-11-28 06:24:34 +0000

    Initial Commit: Added Header, Footer, adn ROUter

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <h1 className="bg-green-600 p-4 text-center">React Router</h1> */}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
